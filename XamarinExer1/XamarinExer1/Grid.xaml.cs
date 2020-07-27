9a421c1889c1a4725a45353b72c958d2d8b65cd8
Martrei <kcrey03@gmail.com>
2020-07-27 20:37:59 +0800

    First Commit

---
+++ XamarinExer1/XamarinExer1/Grid.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinExer1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Grid : ContentPage
+    {
+        public Grid()
+        {
+            InitializeComponent();
+            grid2.IsVisible = false;
+        }
+
+        private void ClickmeBtn_Clicked(object sender, EventArgs e)
+        {
+            if (grid1.IsVisible == true)
+            {
+                grid1.IsVisible = false;
+                grid2.IsVisible = true;
+            }
+            else
+            {
+                grid1.IsVisible = true;
+                grid2.IsVisible = false;
+            }
+           
+                
+            
+        }
+
+     
+    }
+}
